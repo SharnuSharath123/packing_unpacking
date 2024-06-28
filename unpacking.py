de23f7bce92ae78e432d99fd1a3f2e88c85e8ec4
SharnuSharath123 <sharnuburli143@gmail.com>
2024-06-28 21:42:57 +0530

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,8 @@
+# Unpacking : It is phenomenonh of extracting the data from function call function designation.
+
+def unpack(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+
+unpack(*['Hii!', 'oops!', 'Ill!'])
